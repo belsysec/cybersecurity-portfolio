3ded9e266333bdddf5eba34b7bb8266be0c3e379
Belsochukwu Amaonye <belsyamaonye@gmail.com>
2026-07-18 05:19:53 -0400

    Added Suspicious Login Detector project

---
+++ suspicious-login-detector/login_detector.py
@@ -0,0 +1,24 @@
+log_file = "login_log.txt"
+
+with open(log_file, "r") as file:
+    lines = file.readlines()
+failed_count = 0
+failed_users = {}
+for line in lines:
+    if "LOGIN FAILED" in line:
+        failed_count += 1
+        username = line.split()[-1]
+        if username not in failed_users:
+            failed_users[username] = 1
+        else:
+            failed_users[username] += 1
+        print("⚠ Suspicious Login Detected:", line.strip())
+print(f"\nTotal Failed Logins: {failed_count}")
+print("\nFailed Login Summary:")
+for username, attempts in failed_users.items():
+    print(f"{username}: {attempts} failed attempts")
+
+    if attempts >= 3:
+        print("⚠ Possible brute-force attack detected!")
+
+        
